286a042ee0afa9fe756397e88f269919135fa8e4
MayronSouza <mayron.souza@gmz.solutions>
2020-08-31 22:07:37 -0300

    Findando a estilizaçao do Dashboard da aplicaçao, utilizando Styled Components, Published e React Icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,78 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
-import { Title } from "./styles";
+import logoImage from "../../assets/logo.svg";
 
-const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
-};
+import { Title, Form, Repositories } from "./styles";
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImage} alt="Git Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pequisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="test">
+        <img
+          src="https://avatars0.githubusercontent.com/u/41699484?s=400&u=d37cb7c6852e8f1c5cec578cc10d8a54f3253cc2&v=4"
+          alt="Mayron Souza"
+        />
+
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="test">
+        <img
+          src="https://avatars0.githubusercontent.com/u/41699484?s=400&u=d37cb7c6852e8f1c5cec578cc10d8a54f3253cc2&v=4"
+          alt="Mayron Souza"
+        />
+
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="test">
+        <img
+          src="https://avatars0.githubusercontent.com/u/41699484?s=400&u=d37cb7c6852e8f1c5cec578cc10d8a54f3253cc2&v=4"
+          alt="Mayron Souza"
+        />
+
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="test">
+        <img
+          src="https://avatars0.githubusercontent.com/u/41699484?s=400&u=d37cb7c6852e8f1c5cec578cc10d8a54f3253cc2&v=4"
+          alt="Mayron Souza"
+        />
+
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
